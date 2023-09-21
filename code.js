99d4a5411baad8b96608da0e2a17c44b3139cf3d
Dhruv8806 <123929419+Dhruv8806@users.noreply.github.com>
2023-09-20 21:48:54 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function insertionSortReverse(array) {
+    for(var i = array.length - 1; i >= 0; i--) {
+        var val = array[i];
+        var j;
+        for(j = i; j < array.length && array[j+1] < val; j++) {
+          array[j] = array[j+1];
+        }
+        array[j] = val;
+    }
     return array;
 }
+
+console.log(insertionSortReverse([34,-12,93,-46,11,24,75,94,98]));
+
+/*
+Sources Used: 
+Classmate - Ziyu Wang 
+TA - Ali Torabi
+*/
